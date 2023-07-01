75a4170565a16c557d93a8dae2d2a3767db2f24d
BryaN759 <khaledmushahed@gmail.com>
2023-07-02 01:54:41 +0600

    goals crud

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const { errorHandler } = require('./middleware/errorMiddleware.js');
+const connectDB = require('./config/db.js');
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+app.use('/api/goals', require('./routes/goalRouter.js'))
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server is listening on port ${port}....`));
